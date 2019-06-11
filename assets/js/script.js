6e16a49c3071ca92d2892634e3a195f17f6ff611
David Berkheimer <berkheimer112@atlanticbb.net>
2019-06-11 17:37:09 -0400

    added my favorite links

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,8 @@
-// alert("david you did it");
+let bookMarkContainer = document.querySelector("#bookMarksContainer");
+let spinX = document.querySelector("#spinX");
+
+// Toggle Bookmarks
+spinX.addEventListener("click", e => {
+  bookMarkContainer.style.display =
+    bookMarkContainer.style.display === "flex" ? "none" : "flex";
+});
