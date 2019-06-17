5e09f84b9eb43c1709d5a8565075826c71ea3459
David Berkheimer <berkheimer112@atlanticbb.net>
2019-06-17 11:42:17 -0400

    jquery added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,13 @@
-let bookMarkContainer = document.querySelector("#bookMarksContainer");
+let bookMarkWrapper = document.querySelector("#bookMarkWrapper");
 let spinX = document.querySelector("#spinX");
 
 // Toggle Bookmarks
 spinX.addEventListener("click", e => {
-  bookMarkContainer.style.display =
-    bookMarkContainer.style.display === "flex" ? "none" : "flex";
+  // bookMarkContainer.style.display =
+  //   bookMarkContainer.style.display === "flex" ? "none" : "flex";
+  if (bookMarkWrapper.style.display === "none") {
+    $(bookMarkWrapper).slideDown("slow");
+  } else {
+    $(bookMarkWrapper).slideUp("slow");
+  }
 });
