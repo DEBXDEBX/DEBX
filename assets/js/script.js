2b0184c1218667e4169a3ec2634a23d8cd4dcb13
David Berkheimer <berkheimer112@atlanticbb.net>
2019-06-17 17:28:18 -0400

    shortend Jquery

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,11 +3,5 @@ let spinX = document.querySelector("#spinX");
 
 // Toggle Bookmarks
 spinX.addEventListener("click", e => {
-  // bookMarkContainer.style.display =
-  //   bookMarkContainer.style.display === "flex" ? "none" : "flex";
-  if (bookMarkWrapper.style.display === "none") {
-    $(bookMarkWrapper).slideDown("slow");
-  } else {
-    $(bookMarkWrapper).slideUp("slow");
-  }
+  $(bookMarkWrapper).slideToggle("slow");
 });
