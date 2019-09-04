c3f940415d2af182c78daedee49f83c85e8bfdaa
David Berkheimer <berkheimer112@atlanticbb.net>
2019-09-04 18:24:33 -0400

    added click sound to xSpin

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,8 @@
 let bookMarkWrapper = document.querySelector("#bookMarkWrapper");
 let spinX = document.querySelector("#spinX");
-
+let clickAudio = document.querySelector("#clickAudio");
 // Toggle Bookmarks
 spinX.addEventListener("click", e => {
+  clickAudio.play();
   $(bookMarkWrapper).slideToggle("slow");
 });
